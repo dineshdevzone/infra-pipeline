b1ee3392a4015b6f38c8b712dba28bbf669bb6f7
dineshrockey2002 <dineshshanmuga2002@gmail.com>
2026-06-10 11:30:40 +0530

    env spelling mistake

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "infra-terra-buck"
-    key = "enivironments/dev/terraform.tfstate"
+    key = "environments/dev/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
